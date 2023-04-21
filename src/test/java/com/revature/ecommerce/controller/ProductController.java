93d5295251c585206db2b0ac341a35c64fa643ee
connor-eg <connoregarcia@gmail.com>
2023-04-21 12:44:20 -0500

    built my first unit test

--- src/test/java/com/revature/ecommerce/controller/ProductController.java
+++
@@ -1,8 +0,0 @@
-package com.revature.ecommerce.controller;
-
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class ProductController {
-    
-}
---
+++ src/test/java/com/revature/ecommerce/controller/ProductControllerTest.java
@@ -0,0 +1,20 @@
+package com.revature.ecommerce.controller;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.revature.ecommerce.exceptions.SessionTokenInvalidException;
+
+@SpringBootTest
+public class ProductControllerTest {
+    @Autowired
+    private ProductController productController;
+
+    @Test
+    void getItemsThrowsExceptionWithBadToken(){
+        assertThrows(SessionTokenInvalidException.class, () -> {productController.getAllItems("badtoken");}, "The productController accepted a bad token");
+    }
+}
